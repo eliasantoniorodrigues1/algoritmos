06e0b78027c9787f6c5543a9ce723e4bdf5ac7b1
Elias Rodrigues <49626719+eliasantoniorodrigues1@users.noreply.github.com>
2021-06-28 21:55:03 -0300

    Aula de algoritmos PUC Minas

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CalculaHipotenusa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Algoritmo que recebe os valores dos catetos e calcula
+            // a hipotenusa
+            Double cateto1, cateto2, hipotenusa;
+            Console.Write("Digite o valor do Cateto 1: ");
+            cateto1 = float.Parse(Console.ReadLine());
+            Console.Write("Digite o Cateto 2: ");
+            cateto2 = float.Parse(Console.ReadLine());
+
+            hipotenusa = Math.Sqrt(Math.Pow(cateto1, 2) + Math.Pow(cateto2, 2));
+            Console.Write($"O valor da Hipotenusa para os catetos {cateto1} e {cateto2} é de {hipotenusa:0.00}");
+
+        }
+    }
+}
